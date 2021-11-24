4fcf7b0abcce3e10fb6a39188ff67469e1637f6f
Andra Danalachi <>
2021-11-24 12:18:47 +0200

    Add a Switcher component

---
+++ src/components/Switcher.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export function Switcher(props) {
+  const { children, threshold, gap, limit } = props;
+
+  const StyledDiv = styled.div`
+    ${threshold && `--threshold: ${threshold};`}
+    ${gap && `--gap: ${gap};`}
+
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--gap, 1rem);
+
+    & > * {
+      flex-grow: 1;
+      flex-basis: calc((var(--threshold, 40rem) - 100%) * 999);
+    }
+
+    & > :nth-last-child(n + ${limit}),
+    & > :nth-last-child(n + ${limit}) ~ * {
+      flex-basis: 100%;
+    }
+  `;
+
+  return <StyledDiv threshold={threshold}>{children}</StyledDiv>;
+}
